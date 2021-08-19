d7224e05672f8081430bfa0dbef1933222a41dbe
IronO2Caster <burning.nova13@gmail.com>
2021-08-19 09:48:48 +0200

    -Fixed HTML render issue -Completed assignment extras

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,11 +10,13 @@ class HomeController extends Controller
         $weather = new Weather();
         $weatherNijmegen = json_decode($weather->inLanguage('nl')->get('nijmegen'));
         $table = $this->generateTable($weatherNijmegen->main);
+        $grid = $this->generateGrid();
 
 
         return view(
             'welcome',
-            ['weather' => $table]
+            ['weather' => $table],
+            ['grid' => $grid]
         );
     }
 
@@ -28,26 +30,38 @@ class HomeController extends Controller
             $value = round($value);
             $textColor = "";
 
-            switch ($value) {
-                case $value < 20:
-                    $textColor = "text-blue-600";
-                    break;
-                case $value < 22:
-                    $textColor = "text-green-600";
-                    break;
-                case $value >= 22:
-                    $textColor = "text-yellow-600";
-                    break;
-                default:
-                    break;
+            if ((-50 <= $value) && ($value <= 50)) {
+                switch ($value) {
+                    case $value < 20:
+                        $textColor = "text-blue-600";
+                        break;
+                    case $value < 22:
+                        $textColor = "text-green-600";
+                        break;
+                    case $value >= 22:
+                        $textColor = "text-yellow-600";
+                        break;
+                    default:
+                        break;
+                }
             }
 
-            $tableNameRow .= "<td>{$key}</td>";
-            $tableDataRow .= '<td class="' . $textColor . '">' . $value . '</td>';
+            $tableNameRow .= '<td class="p-4 bg-gray-100 font-black uppercase">' . $key . '</td>';
+            $tableDataRow .= '<td class="px-4 py-2 bg-white ' . $textColor . '">' . $value . '</td>';
         }
 
         $tableData .= "<tr>{$tableNameRow}</tr><br><tr>{$tableDataRow}</tr>";
 
         return $tableData;
+    }
+
+    private function generateGrid() {
+        $grid = '';
+
+        for ($i = 1; $i <= 16; $i++) {
+            $grid .= '<div class="p-10 font-bold bg-yellow-500 text-center">' . $i . '</div>';
+        }
+
+        return $grid;
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-center">
-            <div class="space-y-6">
-                <h1 class="text-5xl font-extrabold tracking-wider text-center text-gray-600">
-                    Trinos - Welcome
-                </h1>
 
-                @if ($weather)
-                    <table>
-                        {{$weather}}
-                    </table>
-                @endif
+    <main>
+        <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+            <div class="flex items-center justify-center">
+                <div class="space-y-6">
+                    <h1 class="text-5xl font-extrabold tracking-wider text-center text-gray-600">
+                        Trinos - Welcome
+                    </h1>
 
-                <ul class="list-reset">
-                    <li class="inline px-4">
-                        <a href="https://tailwindcss.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Tailwind CSS</a>
-                    </li>
-                    <li class="inline px-4">
-                        <a href="https://github.com/alpinejs/alpine" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Alpine.js</a>
-                    </li>
-                    <li class="inline px-4">
-                        <a href="https://laravel.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Laravel</a>
-                    </li>
-                </ul>
+                    @if ($weather)
+                        <table>
+                            {!! $weather !!}
+                        </table>
+                    @endif
+
+                    <div class="grid lg:grid-cols-4 md:grid-cols-2 gap-4">
+                        @if($grid)
+                            {!! $grid !!}
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </main>
+
+    <footer class="h-10">
+        <ul class="flex list-reset bg-gray-300 p-2 text-center">
+            <li class="flex-1 inline px-4">
+                <a href="https://tailwindcss.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Tailwind CSS</a>
+            </li>
+            <li class="flex-1 inline px-4">
+                <a href="https://github.com/alpinejs/alpine" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Alpine.js</a>
+            </li>
+            <li class="flex-1 inline px-4">
+                <a href="https://laravel.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Laravel</a>
+            </li>
+            <li class="flex-1 inline px-4">
+                <a href="https://www.w3schools.com/php/" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">W3schools</a>
+            </li>
+        </ul>
+    </footer>
 @endsection
